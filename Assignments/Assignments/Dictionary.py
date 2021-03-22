a09d5570833de5540a1a88fc1ab82185580f8a03
akankshajogdand <81172728+akankshajogdand@users.noreply.github.com>
2021-03-22 21:14:07 +0530

    Add files via upload

---
+++ Assignments/Assignments/Dictionary.py
@@ -0,0 +1,3 @@
+dict={'ID':'E0001','Name':'Rachel Peters','Designation': 'Vice President'}
+dict.update({'ID':'E0006','Name':'Gloria Rogers','Designation':'Admin'})
+print(dict)
---
+++ Assignments/Assignments/userfunc.py
@@ -0,0 +1,39 @@
+#averageofnumbers
+#def avg(x1,x2):
+    #print("Average of given numbers is:",(x1+x2)/2)
+
+#avg(200,230)
+#print(avg)
+
+#x=5 and 7 #(logical)
+#print(x)
+
+#y=5 & 7   #(bitwise)
+#print(y)
+
+#membership operators: in ,not in
+#s="A really long string"
+#print("ally" in s)
+#s="A really long string"
+#print("ally" not in s)
+
+#a=[1,2,3,78,24]
+#print("4" in a)
+
+#identicaloperators: is ,is not
+#print([1,2] is [1,2])
+#print([1,2]==[1,2]) #(comparison operator)
+
+print("Hello" is "HELLO")
+#print("Hello" == "Hello")
+#print("Hello" is not "Hello")
+#b=[34,24]
+#print(b is not a)
+#print(b is a)
+
+#Ternaryoperators:
+#[on.True] if [expression] else [on.False]
+#if() elif()
+#a=10
+#b=20
+#print(a if (a==20) else b)
