a9b2a2f3aedb371db77c61d7ecf89d992ab5ce68
akankshajogdand <81172728+akankshajogdand@users.noreply.github.com>
2021-03-22 21:19:56 +0530

    Delete Assignments directory

--- Assignments/Assignments/Dictionary.py
+++
@@ -1,3 +0,0 @@
-dict={'ID':'E0001','Name':'Rachel Peters','Designation': 'Vice President'}
-dict.update({'ID':'E0006','Name':'Gloria Rogers','Designation':'Admin'})
-print(dict)
--- Assignments/Assignments/foroddeve.py
+++
@@ -1,8 +0,0 @@
-a=[10,11,12,13,14]
-
-for i in range(5):
-    if(a[i]%2==0):
-        print("Number is even")
-    else:
-     print("Number is odd")
-    
